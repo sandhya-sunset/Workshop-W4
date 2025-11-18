000b9ed91cac5892f89f99597c301b0e24297845
sandhya-sunset <sandhyamajhi77@gmail.com>
2025-11-18 11:50:31 +0545

    Task 2

---
+++ Task 2/Calculator.cs
@@ -0,0 +1,22 @@
+﻿namespace Task_2
+{
+    public class Calculator
+    {
+        public void PrintWelcome()
+        {
+            Console.WriteLine("Welcome to the Calculator");
+        }
+
+
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Multiply(int num1, int num2 = 1)
+        {
+            return num1 * num2;
+        }
+    }
+
+}

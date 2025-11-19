1b1d222596a26e8fb5dacecf577cae77e73257c4
sandhya-sunset <sandhyamajhi77@gmail.com>
2025-11-19 11:11:46 +0545

    Task 6

---
+++ Task 6/Program.cs
@@ -0,0 +1,16 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter marks: ");
+        int.TryParse(Console.ReadLine(), out int marks);
+
+        Console.Write("Enter total: ");
+        int.TryParse(Console.ReadLine(), out int total);
+
+        double percentage = (double)marks / total * 100;
+
+        Console.WriteLine("Percentage: " + percentage);
+    }
+}
